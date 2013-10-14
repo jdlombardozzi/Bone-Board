311d88d2b6cbf53cf6895b585200bd9858b6cd2f
Jason Lombardozzi <jason.lombardozzi@gmail.com>
2013-10-14 14:23:07 -0400

    Intial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoneBoard::Application.config.secret_token = '57ec5ed2702585f40cde01b162d09354ea1b3a02b2257bf847a81e5b99b08e8372a37e5e49a06e87c6e401978cfc45ce7a929e47d5a45903334ccdd4c78a6a2f'
